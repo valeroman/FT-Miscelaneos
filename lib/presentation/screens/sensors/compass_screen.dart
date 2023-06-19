8f6ad0ac28f8cb589597c8a941f2c7384d115374
valeroman <valeroman@gmail.com>
2023-06-19 15:58:10 -0500

    depuracion

--- lib/presentation/screens/sensors/compass_screen.dart
+++ lib/presentation/screens/sensors/compass_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:math' show pi;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:miscelaneos/presentation/providers/providers.dart';
